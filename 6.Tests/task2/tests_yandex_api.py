f2877def7b3620c15415042c6eb46c4a4d2cf396
AndreyDyv <94992451+AndreyDyv@users.noreply.github.com>
2023-03-03 01:59:44 +0400

    Update tests_yandex_api.py

--- 6.Tests/task2/tests_yandex_api.py
+++ 6.Tests/task2/tests_yandex_api.py
@@ -17,12 +17,6 @@ class TestYandexDisk(TestCase):
         params = {"path": 'Test_netology_folder', 'permanently': 'true'}
         requests.delete(URL, headers=headers, params=params)
 
-    def tearDown(self):
-        URL = 'https://cloud-api.yandex.net:443/v1/disk/resources'
-        headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.ya.token}'}
-        params = {"path": 'Test_netology_folder', 'permanently': 'true'}
-        requests.delete(URL, headers=headers, params=params)
-
     def test_creation_status_201(self):
         self.assertEqual(self.ya.create_new_folder('Test_netology_folder'), 201)
 
@@ -33,7 +27,12 @@ class TestYandexDisk(TestCase):
     def test_broken_token_401(self):
         self.ya.token = 'broken_token'
         self.assertEqual(self.ya.create_new_folder('Test_netology_folder'), 401)
-
+    
+    def tearDown(self):
+        URL = 'https://cloud-api.yandex.net:443/v1/disk/resources'
+        headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.ya.token}'}
+        params = {"path": 'Test_netology_folder', 'permanently': 'true'}
+        requests.delete(URL, headers=headers, params=params)
 
 if __name__ == '__main__':
     main()
